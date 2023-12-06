fd3308a27a1705a772db0918f29cb9e143206cbf
Sai Krishna <142757076+saikrishna2313@users.noreply.github.com>
2023-12-06 12:27:14 -0800

    Update MyPosts.jsx

--- app/components/MyPosts.jsx
+++ app/components/MyPosts.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useContext } from "react"
+import { useContext,useEffect } from "react"
 import { BlogContext } from "../Context/Context"
 import Image from 'next/image'
 import { useRouter } from "next/navigation"
