822e10c37bb5ad0f7f4fe921d378f6dbae50fef2
Sai Krishna <142757076+saikrishna2313@users.noreply.github.com>
2023-12-06 10:25:01 -0800

    Update page.jsx

--- app/postDetails/[id]/page.jsx
+++ app/postDetails/[id]/page.jsx
@@ -280,7 +280,7 @@ const page = () => {
                                                             return (
                                                                 <section className="w-full flex-col flex gap-1 bg-white">
                                                                     <div className="flex justify-start gap-2 items-center">
-                                                                        <button onClick={() => router.push(`/profile/${comment?.userId}`)}> <Image src={user?.userImage} width={30} height={30} className="h-8 w-8" /></button>
+                                                                        <button onClick={() => router.push(`/profile/${comment?.userId}`)}> <Image src={user?.userImage? user.userImage:profileLogo} width={30} height={30} className="h-8 w-8" /></button>
                                                                         <p className="text-slate-800 font-semibold capitalize">{user?.userName}</p>
                                                                     </div>
                                                                     <p className=" flex border justify-between items-center px-4 shadow-lg w-full py-2 bg-slate-100 text-lg  max-sm:text-sm "><span>{comment.comment}</span>
