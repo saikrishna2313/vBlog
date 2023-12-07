5e1857a6f6bbef0a4afc3056a0725eabdeddff8a
Sai Krishna <142757076+saikrishna2313@users.noreply.github.com>
2023-12-07 07:20:27 -0800

    Update page.jsx

--- app/postDetails/[id]/page.jsx
+++ app/postDetails/[id]/page.jsx
@@ -18,6 +18,7 @@ import { useRouter } from "next/navigation";
 import { IoGameController } from "react-icons/io5";
 import noimage from '../../assets/noimage.png'
 import profileLogo from '../../assets/profileLogo.png'
+import comment from "@/comment";
 const page = () => {
     const router = useRouter()
 
