f20cbdf13077edac0d5bbc8aa7affa184b577f15
Sai Krishna <142757076+saikrishna2313@users.noreply.github.com>
2023-12-07 07:26:37 -0800

    Update page.jsx

--- app/postDetails/[id]/page.jsx
+++ app/postDetails/[id]/page.jsx
@@ -18,7 +18,7 @@ import { useRouter } from "next/navigation";
 import { IoGameController } from "react-icons/io5";
 import noimage from '../../assets/noimage.png'
 import profileLogo from '../../assets/profileLogo.png'
-import comment from "@/app/components/comment";
+import Comment from "@/app/components/Comment"
 const page = () => {
     const router = useRouter()
 
@@ -293,7 +293,7 @@ const page = () => {
                                                    {
                                                         commentsPosts.map((comment) => {
                                                             return (
-                                                                <comment comment={comment} deleteComment={deleteComment} />
+                                                                <Comment comment={comment} deleteComment={deleteComment} />
                                                             )
                                                         })
                                                     }
