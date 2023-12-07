7304bdebbf3c3e38a44266604c82c4c0cc0d6ae3
Sai Krishna <142757076+saikrishna2313@users.noreply.github.com>
2023-12-07 07:45:46 -0800

    Update Comment.jsx

--- app/components/Comment.jsx
+++ app/components/Comment.jsx
@@ -12,13 +12,13 @@ const Comment = ({ commentt, deleteComment }) => {
 
         <section className="w-full flex-col flex gap-1 bg-white">
             <div className="flex justify-start gap-2 items-center">
-                <button onClick={() => router.push(`/profile/${comment?.userId}`)}> <Image src={commentUser?.userImage ? commentUser.userImage : profileLogo} width={30} height={30} className="h-8 w-8" /></button>
+                <button onClick={() => router.push(`/profile/${commentt?.userId}`)}> <Image src={commentUser?.userImage ? commentUser.userImage : profileLogo} width={30} height={30} className="h-8 w-8" /></button>
                 <p className="text-slate-800 font-semibold capitalize">{commentUser?.userName}</p>
             </div>
-            <p className=" flex border justify-between items-center px-4 shadow-lg w-full py-2 bg-slate-100 text-lg  max-sm:text-sm "><span>{comment.comment}</span>
+            <p className=" flex border justify-between items-center px-4 shadow-lg w-full py-2 bg-slate-100 text-lg  max-sm:text-sm "><span>{commentt.comment}</span>
                 {
                     currentUserId === comment.userId && <button onClick={() => {
-                        deleteComment(comment.comment)
+                        deleteComment(commentt.comment)
 
 
                     }}><AiFillDelete className="w-6 h-6" /></button>
